0d89df56e0baa3b4a380ff5dc6540b933b4de9b8
Kellen Erskine <69778229+Kellenerskine@users.noreply.github.com>
2025-04-12 11:04:03 -0400

    stuff changed

---
+++ textbook/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\kelle\Documents\code\Rusty\Textbook_Stuff\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\kelle\Documents\code\Rusty\Textbook_Stuff\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ textbook/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\kelle\Documents\code\Rusty\Textbook_Stuff\variables\target\debug\variables.exe: C:\Users\kelle\Documents\code\Rusty\Textbook_Stuff\variables\src\main.rs
