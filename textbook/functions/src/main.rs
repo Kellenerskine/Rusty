49757056e06f11e5455af4f8950d27f1ba8b93bc
Kellen Erskine <69778229+Kellenerskine@users.noreply.github.com>
2025-04-12 11:11:12 -0400

    added functions dir

---
+++ textbook/functions/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello World!");
+
+    another_function(5);
+}
+
+fn another_function(x: i32) {
+    println!("Another function and {x}");
+}
---
+++ textbook/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\kelle\Documents\code\Rusty\textbook\functions\target\debug\functions.exe: C:\Users\kelle\Documents\code\Rusty\textbook\functions\src\main.rs
