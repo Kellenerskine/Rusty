9d6453b3228368bb685e1e99db196f43ccb7f045
relzwit <kellenae129@gmail.com>
2025-04-20 22:33:50 -0400

    cheat day ig

--- textbook/enums/src/main.rs
+++ textbook/enums/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+enum UpAddrKinda {
+    V4,
+    V6,
 }
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+let home = IpAddr {
+    kind: IpAddrKind::V4,
+    address: String::from("127.0.0.1"),
+};
+
+let loopback = IpAddr {
+    kind: IpAddrKind::V6,
+    address: String::from("::1"),
+};
+
+//not in the mood for this shit. feeling sick lol
+// cheat day i guess
